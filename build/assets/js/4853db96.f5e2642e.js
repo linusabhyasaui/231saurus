cd1f1bdb435e4cee1e6ddba46e2be6e8f6df99d7
linusabhyasaui <linus.abhyasa@ui.ac.id>
2023-05-09 16:47:05 +0700

    Reset and redo DocuSaurus, mistake with build

---
+++ build/assets/js/4853db96.f5e2642e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_231_saurus=self.webpackChunk_231_saurus||[]).push([[6886],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ build/assets/js/cce297e2.e94c164c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_231_saurus=self.webpackChunk_231_saurus||[]).push([[2924],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
